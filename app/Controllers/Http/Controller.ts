0ad0bf85f87ef60d9e7cab30b5317f44e466b343
Thallys Moura <thallys.moura@ham2k3.local>
2022-07-26 16:02:41 -0400

    fix\inclusao_parametros_roteamento

--- app/Controllers/Http/Controller.ts
+++ app/Controllers/Http/Controller.ts
@@ -1,20 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class Controller {
+    /**
+    * Responsável por Recuperar Empresa
+    */
+    
     recuperarEmpresa ({ request, response } : HttpContextContract ) {
-        return {
+        const CODUSUARIO = request.input('CODUSUARIO')
+        response.json({
+            usuario: CODUSUARIO,
             status : true,
-        };
+        })
     }
+
+    /**
+    * LOG-IN do usuário
+    */
+    
 
     login ({ request, response } : HttpContextContract ) {
         const USUARIO = request.input('USUARIO')
         const SENHA   = request.input('SENHA')
 
-        return {
+        response.json({
             user: USUARIO,
             pass: SENHA,
-            auth : true,
-        };
+            status : true,
+        })
     }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -2,8 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
     
-  Route.get   ('/recuperarEmpresa', 'Controller.recuperarEmpresa')
-  Route.post   ('/login',            'Controller.login')
+  Route.get   ('/recuperarEmpresa',   'Controller.recuperarEmpresa')
+  Route.post  ('/login',              'Controller.login')
 
 }).prefix('/api')
 
