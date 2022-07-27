2a6ea417a4de6442f0daa255e9a7e3bc5ce02ed9
Thallys Moura <thallys.moura@ham2k3.local>
2022-07-27 17:19:47 -0400

    feat\migrando_simulacao_oracle

--- app/Controllers/Http/Controller.ts
+++ app/Controllers/Http/Controller.ts
@@ -1,16 +1,28 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class Controller {
     /**
     * Responsável por Recuperar Empresa
     */
     
-    recuperarEmpresa ({ request, response } : HttpContextContract ) {
+    async recuperarEmpresa ({ request, response } : HttpContextContract ) {
         const CODUSUARIO = request.input('CODUSUARIO')
-        response.json({
-            usuario: CODUSUARIO,
-            status : true,
-        })
+        const user = await Database.rawQuery(`
+        SELECT                                                                                       
+            multi_empresas.CD_MULTI_EMPRESA, multi_empresas.DS_MULTI_EMPRESA EMPRESA           
+        FROM                                                                                   
+            DBASGU.USUARIOS                  usuario                                            
+            ,DBAMV.USUARIO_MULTI_EMPRESA     usuario_multi_empresa                              
+            ,DBAMV.MULTI_EMPRESAS            multi_empresas                                     
+            WHERE                                                                                  
+                usuario.CD_USUARIO = ? AND usuario.SN_ATIVO = 'S'                           
+                AND  usuario.CD_USUARIO = usuario_multi_empresa.CD_ID_USUARIO`
+               , [CODUSUARIO])
+        
+            response.json(
+                user
+            )
     }
 
     /**
@@ -21,7 +33,6 @@ export default class Controller {
     login ({ request, response } : HttpContextContract ) {
         const USUARIO = request.input('USUARIO')
         const SENHA   = request.input('SENHA')
-
         response.json({
             user: USUARIO,
             pass: SENHA,
